1f57881c3ad89ea8e96e149b15b578eac1f650d5
belerweb <belerweb@gmail.com>
2013-05-21 13:18:33 +0800

    test $_ENV & $_SERVER

--- info.php
+++ info.php
@@ -3,4 +3,8 @@
 	echo $_ENV["XIUXING.DB.USER"];
 	echo $_ENV["XIUXING.DB.PASSWORD"];
 	echo $_ENV["XIUXING.DB.HOST"];
+	echo "$_ENV";
+	echo $_ENV["TEST"];
+	echo "$_SERVER";
+	echo $_SERVER["TEST"];
 ?>
